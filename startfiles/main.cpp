c15074c409f4577c6dc069739e103ef9932560da
Sumukh Kowndinya <112752323+Sumz-K@users.noreply.github.com>
2024-04-09 15:55:27 +0530

    minor changes

--- startfiles/main.cpp
+++ startfiles/main.cpp
@@ -1,6 +1,6 @@
 #include "Vector.hpp"
 #include <vector>
-
+#include"Map.hpp"
 int main()
 {
     Vector<int> v1 = {10, 5, 6};
@@ -17,6 +17,16 @@ int main()
     
 
     auto res=dot(v1,v2);
-    cout<<res<<" "<<typeid(res).name();
+    cout<<res<<" "<<typeid(res).name()<<"\n";
 
+    // Unordered_Map<int,int> hmap;
+    // hmap.insert(1,2);
+    // cout<<hmap.get(1)<<"\n";
+    // cout<<hmap[1]<<"\n";
+
+    // hmap.insert(2,3);
+    // cout<<hmap[2];
+
+    Vector<string>v3={"hello","no"};
+    v3.print();
 }
