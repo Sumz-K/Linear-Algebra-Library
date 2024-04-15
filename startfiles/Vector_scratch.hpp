e7d2706c39ec808555a56098b981cbb6e659cbf2
Sumukh Kowndinya <112752323+Sumz-K@users.noreply.github.com>
2024-04-15 20:37:56 +0530

    minor changes

--- startfiles/Vector_scratch.hpp
+++ startfiles/Vector_scratch.hpp
@@ -4,15 +4,17 @@
 #define MAX 100
 using namespace std;
 
+
+//container for a vector
 template <typename T>
 class Vector_scratch
 {
 public:
-    T *container;
-    int capacity;
-    int current;
+    T *container;  //the datastore
+    int capacity;  //maxsize
+    int current;   //how many elements currently in the vector
 
-
+//getters
     T* getContainer(){
         return this->container;
     }
@@ -45,6 +47,7 @@ public:
         {
             container[i] = 0;
         }
+        
     }
 
     Vector_scratch(initializer_list<T> list)
@@ -56,6 +59,7 @@ public:
         {
             container[i++] = elem;
         }
+        
         current = capacity;
     }
 
@@ -79,16 +83,30 @@ public:
         }
         cout << "\n";
     }
+    void print() const
+    {
+        if(this->capacity==0){
+            cout<<"No memory allocated to the vector\n";
+            return ;
+        }
+        for (int i = 0; i < current; i++)
+        {
+            cout << container[i] << " ";
+        }
+        cout << "\n";
+    }
 
+
+//custom push back implementation
     void push_back(T data)
     {
         if (current < capacity)
         {
-            container[current] = data;
+            container[current] = data; //if theres extra space in the container just append to it
             current += 1;
         }
         else
-        {
+        { //otherwise create a new container double the size of the older container(as per stl vector), bring back all the elements of the older container and append the new element
             T *expanded = new T[2 * capacity];
             for (int i = 0; i < capacity; i++)
             {
@@ -101,12 +119,13 @@ public:
             current += 1;
         }
     }
-
+//pop back is simply a decrement in number of elements, all accesses of the vector use this variable so popped variables are never accessed
     void pop_back()
     {
         current -= 1;
     }
 
+//similar to pop back,just at the front
     void pop_front()
     {
         for (int i = 0; i < current; i++)
@@ -116,6 +135,7 @@ public:
         current -= 1;
     }
 
+//operator overload to access the elements of the container easily
     T &operator[](int index)
     {
         if (index >= capacity || index < 0)
@@ -148,27 +168,38 @@ public:
         return current;
     }
 
+    int size() const{
+        return current;
+    }
     int maxsize()
     {
         return capacity;
     }
 
+
+//Using a variadic template to push back multiple elements into the vector
     template <typename... Args>
     void push_back_many(Args &&...args)
     {
-        (this->push_back(std::forward<Args>(args)), ...);
+        (this->push_back(std::forward<Args>(args)), ...);  //using forward because the elements may be lvalue or rvalue references and we want to handle both
     }
 
+    template<int sz>
     T accumulate() const
-    {
-        return accumulate_helper(std::make_index_sequence<MAX>{});
+    { //to calculate sum of all elements of an array we use a fold expression
+        return accumulate_helper(std::make_index_sequence<sz>{}); 
+
     }
 
     template <size_t... Is>
-    T accumulate_helper(std::index_sequence<Is...>) const
-    {
+    T accumulate_helper(std::index_sequence<Is...>) const //fold expression being used, index sequence to convert the elements of the array to indiviudal elements
+    { 
         return (container[Is] + ...);
     }
+
+    
+
+    //For some reason the destructor was buggy hence created a custom function to de-allocate memory from a vector
     void del()
     {
         
@@ -183,6 +214,5 @@ public:
         this->capacity=0;
         delete[] container; 
     }
-
 
 };
